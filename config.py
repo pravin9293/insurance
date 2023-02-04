a5827be500170b89fabebbb49a244052ca421cd1
unknown <pravinlinge92@gmail.com>
2023-02-04 22:56:38 +0530

    Config File Modified

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ CSV_FILE_PATH = r'CSV_Files\medical_insurance.csv'
 
 MODEL_FILE_PATH = r'artifacts/Model.pkl'
 
-JSON_FILE_PATH = r'artifacts\project_data.json'
+JSON_FILE_PATH = r'artifacts/project_data.json'
 
-SCALER_MODEL = r'artifacts\normal_scaler.pkl'
+SCALER_MODEL = r'artifacts/normal_scaler.pkl'
 
 PORT_NUMBER = 1234
