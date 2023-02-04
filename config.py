e3fd7b1045d8ae9bfac87ff03d5b01215be261d9
unknown <pravinlinge92@gmail.com>
2023-02-04 22:47:29 +0530

    config file modified

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 CSV_FILE_PATH = r'CSV_Files\medical_insurance.csv'
 
-MODEL_FILE_PATH = r'artifacts\Model.pkl'
+MODEL_FILE_PATH = r'artifacts/Model.pkl'
 
 JSON_FILE_PATH = r'artifacts\project_data.json'
 
